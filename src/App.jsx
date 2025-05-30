4a7467b6c02cf843211ff8eac72012da6883a90f
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2025-05-30 08:29:12 +0300

    Add new components and update App structure for enhanced functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import CoffeeForm from "./components/CoffeeForm"
+import Hero from "./components/Hero"
+import History from "./components/History"
 import Layout from "./components/Layout"
+import Stats from "./components/Stats"
 
 function App() {
 
   const isAuthenticated = false
+
+  const authenticatedcontent = (
+    <>
+      <Stats />
+      <History />
+    </>
+  )
   
 
   return (
     <Layout>
-      
+      <Hero />
+      <CoffeeForm />
+      {isAuthenticated && (authenticatedcontent) }
     </Layout>
   )
 }
