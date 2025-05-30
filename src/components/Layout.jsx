6c4af1dacbe81b4afa42ef302de2edd40c3d07ec
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2025-05-30 08:58:50 +0300

    Adds header and footer to Layout component
    Implements a new header with a title and sign-up button, enhancing the user interface.
    
    Replaces the empty footer with a credit to the developer, providing more information about the project.
    
    These changes aim to improve the overall presentation and user engagement of the layout.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,9 +3,22 @@ export default function Layout(props){
     const {children} = props
 
     const header = (
-        <header></header>
+        <header>
+            <div>
+                <h1 className="text-gradient">CAFFIEND </h1>
+                <p>for coffee instatiates</p>
+            </div>
+            <button>
+                <p>Sign up free</p>
+                <i className="fa-solid fa-mug-hot"></i>
+            </button>
+        </header>
     )
-    const footer = (<footer></footer>)
+    const footer = (
+        <footer>
+            <p><span className="text-gradient">Caffiend</span> was made by <a href="https://developersam-portfolio.netlify.app/">Sam Waithaka</a></p>
+        </footer>
+    )
     return(
         <>
             {header}
