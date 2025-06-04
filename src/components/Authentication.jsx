19c70f99a6cefb7da0e8e65f201c295d963036a0
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2025-06-04 14:26:50 +0300

    Adds state management for authentication component
    Implements state management for email, password, and registration toggle in the authentication component.
    
    Enhances user experience by dynamically updating the UI for sign-up and login processes.
    
    Prepares for future functionality by adding an asynchronous authentication handler.

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,20 +1,27 @@
-
+import { useState } from "react"
 
 export default function Authentication(){
-    
+    const [isRegistration, setIsRegistration] = useState(false)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isAuthenticating, setIsAuthenticating] = useState(false)
 
+    async function handleAuthenticate (){
+
+    }
+    
     return (
         <>
-            <h2 className="sign-up-text">Sign Up / Login</h2>
-            <p>Sign in to your account</p>
-            <input placeholder="Email" />
-            <input placeholder="********" type="password" />
-            <button><p>Submit</p></button>
+            <h2 className="sign-up-text">{ isRegistration ? 'Sign Up' : 'Login'}</h2>
+            <p>{isRegistration ? 'Create an account': "Sign in to your account"}</p>
+            <input value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder="Email" />
+            <input value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder="********" type="password" />
+            <button onClick={handleAuthenticate} ><p>Submit</p></button>
             <hr />
 
             <div className="register-content">
-                <p>Don&apos;t have an account?</p>
-                <button><p>Sign Up</p></button>
+                <p>{isRegistration ? 'Already Have an account?': "Don't have an account?"}</p>
+                <button onClick={()=>{setIsRegistration(!isRegistration)}}><p>{isRegistration ? 'Sign In' : 'Sign Up'}</p></button>
             </div>  
         </>
     )
