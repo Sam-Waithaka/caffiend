a0dd808a1f78e7fb6f338b9df733ca22257f1146
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2025-06-05 10:10:35 +0300

    refactor: update authentication logic in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Hero from "./components/Hero"
 import History from "./components/History"
 import Layout from "./components/Layout"
 import Stats from "./components/Stats"
+import { useAuth } from "./context/AuthContext"
 
 function App() {
-
-  const isAuthenticated = false
+  const {globalUser, isLoading, globalData} = useAuth()
+  const isAuthenticated = globalUser
+  const isData = globalData && !!Object.keys(globalData || {}).length
 
   const authenticatedcontent = (
     <>
@@ -20,7 +22,10 @@ function App() {
     <Layout>
       <Hero />
       <CoffeeForm isAuthenticated={isAuthenticated}/>
-      {isAuthenticated && (authenticatedcontent) }
+      {(isLoading && isAuthenticated ) && (
+        <p>Loading data...</p>
+      )}
+      {(isAuthenticated && isData) && (authenticatedcontent) }
     </Layout>
   )
 }
